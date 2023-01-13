84be72c085cd8b27d99ca091e29590be9537f40b
Rahulpatel78 <109850702+Rahulpatel78@users.noreply.github.com>
2023-01-13 18:45:38 +0530

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 
-model  = pickle.load(open('C:\Users\rahul\PycharmProjects\car_price_prediction\LinearRegressionmode1.pkl' , 'rb'))
+model  = pickle.load(open(f'C:\Users\rahul\PycharmProjects\car_price_prediction\LinearRegressionmode1.pkl' , 'rb'))
 
 def main():
     st.title('car price prediction')
